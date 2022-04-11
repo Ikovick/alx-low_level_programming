ec33dbf7d70c84eede8267855ad944a848fcd084
Ikovick <ikovick@yahoo.com>
2022-04-11 13:48:20 -0700

    My commit message

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct
+ */
+int main(void)
+
+{
+int I:
+
+for (i = 48 ;mi <= 57; i++)
+{
+putchar (i);
+}
+
+put char ('\n');
+
+
+return (0);
+
+}
